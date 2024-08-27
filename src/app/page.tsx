2b63bf607a1a1140479f389bc988e00bfcdfffb5
yellowstonepark <otzarjaffe@gmail.com>
2024-08-26 20:35:39 -0400

    minor adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default async function Home() {
       <div className='m-6 pr-10 flex-col w-screen h-screen'>
         <div className='inter-extralight text-7xl sm:text-8xl'>Resources</div>
         <div className='flex sm:flex-row flex-col h-full'>
-          <div className='flex flex-col justify-center sm:w-[50vw] w-[80vw] pr-6 h-fit'>
+          <div className='flex flex-col justify-center sm:w-[50vw] w-[80vw] pr-6 h-fit pl-5'>
             <div className='inter-extralight text-2xl'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
@@ -64,7 +64,7 @@ export default async function Home() {
       <div className='m-6 pr-10 flex-col w-screen h-screen'>
         <div className='inter-extralight text-7xl sm:text-8xl'>Events</div>
         <div className='flex sm:flex-row flex-col-reverse sm:h-full h-fit'>
-          <div className='flex flex-col justify-center sm:w-[50vw] w-[80vw] pr-6 h-fit'>
+          <div className='flex flex-col justify-center sm:w-[50vw] w-[80vw] pr-6 h-fit pl-5'>
             <div className='inter-extralight text-2xl'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
