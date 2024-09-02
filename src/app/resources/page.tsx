829cbcb67086b81c725258683aaea31e22606be0
yellowstonepark <otzarjaffe@gmail.com>
2024-09-02 15:05:14 -0400

    resource section

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -7,6 +7,7 @@ import Image1Right from "public/images/ResourceImageRight1.png";
 import Image2Right from "public/images/ResourceImageRight2.png";
 import MobileResourceHero from "public/images/MobileResourceHero.svg";
 import Doge from "public/images/DogeBoy.png"
+import findOutArrow from "public/images/FindOutArrow.svg"
 
 export default function ResourcesPage() {
     return (<div>
@@ -33,8 +34,47 @@ export default function ResourcesPage() {
                 Resources
             </div>
         </div>
-        <div className="w-screen h-screen">
+        <div className="w-screen min-h-{50vh}">
+            <div className="grid p-10">
+                <div className="grid grid-cols-1 gap-10 md:grid-cols-[1.5fr_3fr] md:items-start pb-10 md:pl-10 md:pr-10 pl-5 pr-5 pb-20">
+                    <div className="resourceNameContainer h-full flex md:justify-center p-0">
+                        <div className="h-fit text-4xl md:text-6xl inter-regular p-0">Resource Name</div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <p className="inter-extralight text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <div className="transition-transform hover:translate-x-1 cursor-pointer">
+                            <a href="link-to-item-1" className="inter-bold underline">Link To Item 1</a>
+                            <Image alt="arrow to find out more" loading="lazy" width="48" height="48" decoding="async" data-nimg="1" className="inline-block ml-1 w-6 h-6 " src={findOutArrow} />
+                        </div>
+                    </div>
+                </div>
 
+                <div className="grid grid-cols-1 gap-10 md:grid-cols-[1.5fr_3fr] md:items-start pb-10 md:pl-10 md:pr-10 pl-5 pr-5 pb-20">
+                    <div className="resourceNameContainer h-full flex md:justify-center">
+                        <div className="h-fit text-4xl md:text-6xl inter-regular">Resource Name</div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <p className="inter-extralight text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <div className="transition-transform hover:translate-x-1 cursor-pointer">
+                            <a href="link-to-item-1" className="inter-bold underline">Link To Item 2</a>
+                            <Image alt="arrow to find out more" loading="lazy" width="48" height="48" decoding="async" data-nimg="1" className="inline-block ml-1 w-6 h-6 " src={findOutArrow} />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-1 gap-10 md:grid-cols-[1.5fr_3fr] md:items-start pb-10 md:pl-10 md:pr-10 pl-5 pr-5 pb-20">
+                    <div className="resourceNameContainer h-full flex md:justify-center">
+                        <div className="h-fit text-4xl md:text-6xl inter-regular">Resource Name</div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <p className="inter-extralight text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <div className="transition-transform hover:translate-x-1 cursor-pointer">
+                            <a href="link-to-item-1" className="inter-bold underline">Link To Item 3</a>
+                            <Image alt="arrow to find out more" loading="lazy" width="48" height="48" decoding="async" data-nimg="1" className="inline-block ml-1 w-6 h-6 " src={findOutArrow} />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>);
 }
