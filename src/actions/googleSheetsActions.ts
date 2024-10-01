b90ddb4257edbf5e9990d3993f63160e44c7e539
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-09-30 21:21:33 -0400

    fix skibidi problems

--- src/actions/googleSheetsActions.ts
+++ src/actions/googleSheetsActions.ts
@@ -1,8 +1,22 @@
 "use server";
 import { google } from "googleapis";
 
+const credentials = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+};
+
 export async function getMemberData(sheetID: string) {
   const auth = await google.auth.getClient({
+    credentials: credentials,
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
 
@@ -18,6 +32,7 @@ export async function getMemberData(sheetID: string) {
 
 export async function getNamefromAA(sheetID: string) {
   const auth = await google.auth.getClient({
+    credentials: credentials,
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
 
@@ -33,6 +48,7 @@ export async function getNamefromAA(sheetID: string) {
 
 export async function getDeptPhoto(sheetID: string) {
   const auth = await google.auth.getClient({
+    credentials: credentials,
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
 
@@ -48,6 +64,7 @@ export async function getDeptPhoto(sheetID: string) {
 
 export async function getResources() {
   const auth = await google.auth.getClient({
+    credentials: credentials,
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
   const sheets = google.sheets({ version: "v4", auth });
@@ -62,6 +79,7 @@ export async function getResources() {
 
 export async function getEvents() {
   const auth = await google.auth.getClient({
+    credentials: credentials,
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
 
